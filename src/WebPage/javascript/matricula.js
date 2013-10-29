d44d70209ffc29a84bd3a32b74b78311dde1679c
Iury Gregory Melo Ferreira <iurygregory@gmail.com>
2013-10-29 14:59:00 -0200

    US1 - Webpage

--- src/WebPage/javascript/matricula.js
+++ src/WebPage/javascript/matricula.js
@@ -1,23 +1,61 @@
 var aluno = "";
 var dados_notas = [];
 
-function getMatricula(selection){
-	//aluno = selection.options[selection.selectedIndex].value;
-	//console.log(aluno);
+
+/*Funcao para mostrar a div de competencia*/
+function showcompetencia(){
+	loadData();
+	$("#id_desempenho").hide();
+	$("#id_competencia").show();
+}
+
+/*Funcao para mostrar a div de desempenho*/
+function showdesempenho(){
+	$("#id_competencia").hide();
+	$("#id_desempenho").show();
+}
+
+
+/*Funcao que seleciona um aluno*/
+function getCompetencia(selection){
+	//$("#id_competencia") -- limpar texto da div
+	aluno = selection.options[selection.selectedIndex].value;
+	console.log(aluno);
+	$("#id_competencia").empty();
+	competencia(aluno); 
+}
+
+function getDesempenho(selection){
+	aluno = selection.options[selection.selectedIndex].value;
+	console.log(aluno);	
 }
 
 function loadData(){
 	d3.csv("dados/matriculas.csv" , function (data){	
 		dados_notas = data;		
 		var mat = data.map(function(d){return d.matricula;});
-		var matriculaList = d3.selectAll("#matriculaList");
+		var mycompetencia = d3.selectAll("#mycompetencia");
 		
-		matriculaList.selectAll("option").data(mat).enter().append("option")
+		mycompetencia.selectAll("option").data(mat).enter().append("option")
 		.attr("value",function(d){return d;})
 		.attr("label",function(d){return d;})
-		.attr("data-icon", "icon-map-marker")
 		.text(function(d){return d+"";});
 		
 		$('.selectpicker').selectpicker({'selectedText': 'cat'});
+	});
+}
+
+/*Funcao que filtra as competencias de um aluno*/
+function competencia(id_aluno){
+	d3.csv("dados/competencia.csv", function(data){
+		var file_comp = data;
+		var contentArray = file_comp.filter(function(d){return d.matricula == id_aluno;}); // filtra apenas para ter as disciplinas
+		var otp = '';
+		$.each(contentArray, function(i,val){
+			otp+= " Faz parte dos " +	val.taxa + " na disciplina " + val.disciplina +"<br>";// mudar de acordo com o nome no JSON
+
+		});	
+		var todas_competencias = "<ul> <li> Informacoes de Competencia do aluno: "+id+" </li> <br>" + otp+ " </ul>";
+		$("#id_competencia").append(todas_competencias);
 	});
 }
