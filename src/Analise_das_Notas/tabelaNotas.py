859de6b9f3b467f619ee40d288401d173f839313
Diego Pedro <diegopedrosn@gmail.com>
2013-11-25 17:25:57 -0300

    notas definitivas com muita definicao

--- src/Analise_das_Notas/tabelaNotas.py
+++ src/Analise_das_Notas/tabelaNotas.py
@@ -10,17 +10,46 @@ arquivo2 = open("notas_normalizadas.csv",'w')
 
 
 disciplinas =  {}
+disciplinasQuantidade = {}
 alunos = {}
 erros = [0 for i in xrange(10)]
 dist = [[] for i in xrange(10)]
 distTotal = []
+disciplinasDuasNotas = ["TECC(DIDATICA EM CIENCIA DA COMPUTACAO II)","BASQUETEBOL - FEM"
+			,"Futsal","Teoria dos Grafos","Informática e Sociedade","Paradigmas de Linguagem de Programação"]
+
+
+def calculaQuantidadeNotas():
+	#EQUACOES DIFERENCIAIS:20122:3
+	#Projeto em Computação II:20111:3
+	#Projeto em Computação II:20121:3
+
+	arquivo = open("disc_notas.csv").readlines()
+
+	for linha in arquivo:
+		linhaS = linha.replace("\n","").replace("\r","").split(":")
+		disciplina = linhaS[0]
+		periodo = linhaS[1]
+		quantidade = linhaS[2]
+		
+		if disciplina not in disciplinasQuantidade:
+			disciplinasQuantidade[disciplina] = {}
+
+		if periodo not in disciplinasQuantidade[disciplina]:
+			disciplinasQuantidade[disciplina][periodo] = quantidade
+		
+def getQuantidade(disciplina,periodo):
+	return 	int(disciplinasQuantidade[disciplina][periodo])
+
+calculaQuantidadeNotas()
+
 for linha in arquivo:
 	linhaS = linha.replace("\n","").replace("\r","").split(";")
 	periodo = "".join(linhaS[0:2])	
 	disciplina = linhaS[3]
 	matricula = linhaS[4]
 	prova = linhaS[5]
-	nota = -1.0
+	nota = 0.0
 	media = -1.0
 	situacao = linhaS[7]
 	aluno = linhaS[4]
@@ -57,22 +86,28 @@ for disciplina, dados in disciplinas.iteritems():
 					alunos[aluno][disciplina] = {}
 				if periodo not in alunos[aluno][disciplina]:
 					alunos[aluno][disciplina][periodo] = {}
-				if prova not in alunos[aluno][disciplina][periodo]:
+				if prova not in alunos[aluno][disciplina][periodo]:					
 					alunos[aluno][disciplina][periodo][prova] = (nota,media)
+
+
+
+
+	
 		
+#Um aluno contem disciplinas
+#Uma disciplina contem periodos
+#Um periodo contem provas
+#uma prova contem a nota e a media(gambiarra)
 
 def estimateMedia(registro_notas,curso,periodo):
-	cont = 0
+	cont = getQuantidade(curso,periodo)
 	n1 = 0
 	n2 = 0
 	n3 = 0
 	media = registro_notas[9]
-
-	for i in range(0,8):
-		if registro_notas[i] != -1.0:
-			cont += 1
 	
 	if registro_notas[-2] == -1.0 and registro_notas[-1] >= 7.0:
+		
 		if cont == 4:
 			if curso == "Sistemas de Informação I" and periodo == "20111":
 				n1 = (registro_notas[0])
@@ -129,47 +164,63 @@ def estimateMedia(registro_notas,curso,periodo):
 			erros[8] += dist[8][-1]
 			return (n1,n2,n3,dist[8][-1],media,"(N1+N2+N3)/3 , (N4+N5)/2 , (N6+N7+N8)/3")
 	return ()
+
+#Um aluno contem disciplinas
+#Uma disciplina contem periodos
+#Um periodo contem provas
+#uma prova contem a nota e a media(gambiarra)
 	
-for aluno, dados in alunos.iteritems():
-	for disciplina, dados2 in dados.iteritems():
-		for periodo, dados3 in dados2.iteritems():
-			notas_prova = [-1.0 for i in xrange(10)]				
+for aluno, dados in alunos.iteritems(): #itera sobre os alunos
+	for disciplina, dados2 in dados.iteritems(): #itera sobre as disciplinas do aluno
+		for periodo, dados3 in dados2.iteritems(): #itera sobre os periodos da disciplina do aluno
+
+			notas_prova = [0.0 for i in xrange(10)] #armazena todas as notas da disciplina do periodo corrente				
 			#print list(dados3.iterkeys()),aluno,disciplina,periodo
-			notas_prova[-1] = dados3[list(dados3.iterkeys())[0]][1]
+			notas_prova[-2] = -1.0
+			notas_prova[-1] = dados3[list(dados3.iterkeys())[0]][1] #pega a media
+			if notas_prova[-1] >  0.0:				
 
-			for p in range(max(len(list(dados3.iterkeys())),10)):
-				prova = p
+				#print dados3[list(dados3.iterkeys())[0]] #pega a final
 
-                                if p < len(list(dados3.iterkeys())):
-					prova = list(dados3.iterkeys())[p]
+				for p in range(max(len(list(dados3.iterkeys())),10)): #itera sobre todas as provas (mesmo que nao tenha nota)
+					prova = p
+
+		                        if p < len(list(dados3.iterkeys())): # se a prova atual e inferior a quantidade de provas da disciplina
+						prova = list(dados3.iterkeys())[p]
 					
-					if prova == "F":
-						notas_prova[-2] = float(dados3[prova][0])
+						if prova == "F": #se a prova e uma final
+							notas_prova[-2] = float(dados3[prova][0])
+						else: #caso contrario armazene a nota da prova
+							notas_prova[int(prova)-1] = dados3[prova][0]
+				#print notas_prova			
+				#for n in notas_prova:
+				#	arquivo2.write(str(n)+":")
+				#arquivo2.write(" :")
+						
+				notasEstimadas = list(estimateMedia(notas_prova,disciplina,periodo))
+				if len(notasEstimadas) == 0:
+				     for nota in range(3):
+					arquivo2.write("%1.2f:" % notas_prova[nota])
+				else:
+					for nota in range(2):
+						arquivo2.write("%1.2f:" % notasEstimadas[nota])
+					if disciplina in disciplinasDuasNotas:
+						arquivo2.write("%1.2f:" % notas_prova[-1])
 					else:
-						notas_prova[int(prova)-1] = dados3[prova][0]
-			#print notas_prova			
-			#for n in notas_prova:
-			#	arquivo2.write(str(n)+":")
-			#arquivo2.write(" :")
-			
-			notasEstimadas = list(estimateMedia(notas_prova,disciplina,periodo))
-			if len(notasEstimadas) == 0:
-				for nota in range(3):
-					arquivo2.write(str(notas_prova[nota])+":")
-			else:
-				for nota in range(3):
-					arquivo2.write(str(notasEstimadas[nota])+":")
-			arquivo2.write(str(notas_prova[-2])+":")
-			arquivo2.write(str(notas_prova[-1])+":")
-			#for nota in list(estimateMedia(notas_prova,disciplina,periodo)):					
-			#	arquivo2.write(str(nota)+":")		
-			#print list(estimateMedia(notas_prova))	
-			arquivo2.write(periodo+":"+disciplina+":"+aluno+"\n")
+						arquivo2.write("%1.2f:" % notasEstimadas[2])
+				#arquivo2.write("\n")
+				arquivo2.write("%1.2f:" % notas_prova[-2])
+				arquivo2.write("%1.2f:" % notas_prova[-1])
+				#for nota in list(estimateMedia(notas_prova,disciplina,periodo)):					
+				#	arquivo2.write(str(nota)+":")		
+				#print list(estimateMedia(notas_prova))	
+				arquivo2.write(periodo+":"+disciplina+":"+aluno+"\n")
+				
 
 
-for i in xrange(len(erros)):
+for i in xrange(len(erros)):	
 	if len(dist[i]) > 0:
-		print i,erros[i],len(dist[i]),erros[i]/len(dist[i]),min(dist[i]),max(dist[i])
+		print i,erros[i],len(dist[i]),erros[i]/len(dist[i]),min(dist[i]),max(dist[i])		
 		distTotal = distTotal + dist[i]
 
 arqdist = open("DistribuicaoTotal.csv",'w')
