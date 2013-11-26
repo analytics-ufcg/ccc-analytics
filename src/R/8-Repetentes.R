e36e1b0a2facf8e76163edac4256c9af4a5ed1e2
Larissa Costa <larissa.n.costa@gmail.com>
2013-11-26 11:23:40 -0300

    Dados da repetencia atualizados

--- src/R/8-Repetentes.R
+++ src/R/8-Repetentes.R
@@ -4,8 +4,8 @@ library(plyr)
 d = read.table("../../data/arquivo_notas_disciplinas.csv",sep = ",", header=T)
 
 # Selecionando os alunos que cursaram alguma disciplina mais de uma vez
-parte1 <- duplicated(d[11:13], fromLast = T)
-parte2 <- duplicated(d[11:13], fromLast = F)
+parte1 <- duplicated(d[6:7], fromLast = T)
+parte2 <- duplicated(d[6:7], fromLast = F)
 parte1 <- d[parte1, ]
 parte2 <- d[parte2, ]
 repetentes <- rbind(parte1,parte2)
@@ -13,12 +13,12 @@ repetentes <- repetentes[order(repetentes$matricula),]
 
 
 matriculas <- unique(d$matricula)
-matriculados                     
+                     
 # Funcao para salvar um data frame
 gravarArquivo <- function(dados, destino){
   write.table(dados, file = destino, col.names = TRUE, row.names=FALSE, fileEncoding = "UTF-8")  
 }
 
 # Salvando o arquivo das competencias
-gravarArquivo(a <- na.omit(repetentes), "../../data/repentecia.csv")
+gravarArquivo(a <- na.omit(repetentes), "../../data/repetencia.csv")
 gravarArquivo(a <- na.omit(matriculas), "../../data/matriculasRepetente.csv")
