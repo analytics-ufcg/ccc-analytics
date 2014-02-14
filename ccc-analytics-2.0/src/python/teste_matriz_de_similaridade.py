e0cc2d68333f75c9b8288011c4b51cd4485edc40
Tercio de Melo Sousa <tercio@teste-t.(none)>
2014-02-14 10:33:53 -0300

    analise das metricas utilizadas

--- ccc-analytics-2.0/src/python/teste_matriz_de_similaridade.py
+++ ccc-analytics-2.0/src/python/teste_matriz_de_similaridade.py
@@ -46,14 +46,15 @@ resultado = {} # {(matricula_a, matricula_b) : (distancia, jaccard)}
 mk_key = lambda t3: t3[1:] if t3[1] < t3[2] else tuple(reversed(t3[1:]))
 
 distancia = reduce(lambda x,y: x+y, sim_matrix(mk_mapa_distancia(), 16, 'distancia'))
-
 for t in distancia:
     resultado[mk_key(t)] = (t[0],)
-
 del distancia
+
+
 jaccard = reduce(lambda x,y: x+y, sim_matrix(mk_mapa_jaccard(), 16, 'jaccard'))
-
-
-
 for t in jaccard:
-    resultado(mk_key(t)) += (t[0],)
+    resultado[mk_key(t)] += (t[0],)
+del jaccard
+
+for i in resultado:
+    print i[0], i[1], resultado[i][0], resultado[i][1]
