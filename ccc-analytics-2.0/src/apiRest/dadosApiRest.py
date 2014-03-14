3f00a3fff3ad7eccf052bb64ddce2e5739635733
Tales Tenorio de Souza Pimentel <tales.tsp@gmail.com>
2014-03-14 16:43:08 -0200

    Update dadosApiRest.py
    Corrigindo, agora sim lê da database em Vertica

--- ccc-analytics-2.0/src/apiRest/dadosApiRest.py
+++ ccc-analytics-2.0/src/apiRest/dadosApiRest.py
@@ -1,39 +1,59 @@
 import csv
 import json
+import pyodbc
+
+def create_connection():
+    return pyodbc.connect("DSN=VerticaDSN")
 
 def disciplinas_por_periodo():
-	with open('../../ccc2/data/grade-disciplinas-por-periodo.csv', 'rb') as csvfile:
-		spamreader = csv.reader(csvfile, delimiter=',',quotechar='\n')
-		response = montaJson(spamreader)
-		return json.dumps(response)
+    cnxn = create_connection()
+    cursor = cnxn.cursor()
+    cursor.execute("select * from GradeDisciplinasPorPeriodo")
+    rows = cursor.fetchall()
+    cnxn.close()
+    lista_tuplas = []
+    for tupla in rows:
+       lista_tuplas.append(tupla)
+    col = ["codigo", "periodo", "disciplina"]
+    response = montaJson(lista_tuplas, col)
+    return json.dumps(response)
+
 
 def pre_requisitos():
-	with open('../../ccc2/data/prereq.csv', 'rb') as csvfile:
-		spamreader = csv.reader(csvfile, delimiter=',',quotechar='\n')
-		response = montaJson(spamreader)
-		return json.dumps(response)
+    cnxn = create_connection()
+    cursor = cnxn.cursor()
+    cursor.execute("select * from PreRequisitosDisciplina")
+    rows = cursor.fetchall()
+    cnxn.close()
+    lista_tuplas = []
+    for tupla in rows:
+       lista_tuplas.append(tupla)
+    col = ["codigo", "codigoPreRequisito"]
+    response = montaJson(lista_tuplas, col)
+    return json.dumps(response)
 
 
 def maiores_frequencias():
-	with open('../../ccc2/data/maiores_frequencias_por_disciplina_ordenado_obrigatorias.csv', 'rb') as csvfile:
-		spamreader = csv.reader(csvfile, delimiter=',',quotechar='\n')
-		response = montaJson(spamreader)
-		return json.dumps(response)
-		
+    cnxn = create_connection()
+    cursor = cnxn.cursor()
+    cursor.execute("select * from MaioresFrequenciasPorDisciplinaOrdenadoObrigatorias")
+    rows = cursor.fetchall()
+    cnxn.close()
+    lista_tuplas = []
+    for tupla in rows:
+       lista_tuplas.append(tupla)
+    col = ["codigo", "disciplina", "periodoMaisFreq1st", "freqRelativa1st", "periodoMaisFreq2nd", "freqRelativa2nd", "periodoMaisFreq3rd", "freqRelativa3rd", "totalDeAlunos"]
+    response = montaJson(lista_tuplas, col)
+    return json.dumps(response)
 
-def montaJson(spamreader):
+def montaJson(spamreader, col):
 	response = []
-	colunas = []
+	colunas = col
 	i = 0
 	for row in spamreader:
-		if (i == 0):
-			for column in row:
-				colunas.append(column)		
-		else:
-			celulas = {}
-			for indexColumns in range(0,len(colunas)):
-				celulas[colunas[indexColumns]] = row[indexColumns]
-			response.append(celulas)
+		celulas = {}
+		for indexColumns in range(0,len(colunas)):
+			celulas[colunas[indexColumns]] = row[indexColumns]
+		response.append(celulas)
 		i = i + 1;
 	return response
-
