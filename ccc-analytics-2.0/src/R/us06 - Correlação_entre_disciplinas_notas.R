1718dd0ccf80ddd621f978cfc49264fd8ec595d2
Andryw Marques Ramos <andryw@pearson.(none)>
2014-03-25 17:53:15 -0300

    Modificação do script que calcula correlacao entre disciplinas. Funcionando com dados novos e calculando correlacao de spearman e kendall. Arquivos com correlacao de kendall e spearman

--- ccc-analytics-2.0/src/R/us06 - Correlação_entre_disciplinas_notas.R
+++ ccc-analytics-2.0/src/R/us06 - Correlação_entre_disciplinas_notas.R
@@ -3,6 +3,33 @@
 #    o fluxograma do curso deve mostrar que pares disciplinas têm correlação nas notas dos alunos que as cursaram. 
 #    O sistema deve informar tanto correlação positiva quanto negativa. Deve haver uma escala divergente que mostra isso claramente. 
 #    Ao sobrevoar uma disciplina, ela e suas correlacionadas devem se destacar.
+library(reshape)
+formataMatrizCorrelacao <- function(matrizCorrelacao,grade,value1,value2){
+  data2 = matrizCorrelacao
+  for (i in 2:length(matrizCorrelacao)) {
+    data2[i-1,2:i] = NA
+  }
+  data = data2
+  
+  
+  
+  mdata <- melt(data, id=c("X"))
+  mdata$X <- substring(mdata$X, 7)
+  mdata$variable <- substring(mdata$variable, 7)
+  mdata <- merge(mdata,grade,by.x="X",by.y="CODIGO")
+  mdata <- merge(mdata,grade,by.x="variable",by.y="CODIGO")
+  
+  mdata1 <- subset(mdata,X != variable)
+  mdata2<- subset(mdata1,value > value1 | value <value2)
+  mdata2 <- mdata2[with(mdata2, order(-value)), ]
+  colnames(mdata2) <- c("cod1","cod2","cor","disc1","disc2")
+  mdata3<- subset(mdata2,cor != 1.0 & cor != -1.0)
+  
+  mdata3
+}
+
+
+
 
 
 #setwd("C:/Users/bac/Desktop/CCC Correlacao")
@@ -20,6 +47,7 @@ completeData <- merge(notas,infoAssi[c("CODIGO","TIPODEDISCIPLINA")],by="CODIGO"
 # filtra o data frame com as disciplinas obrigatorias
 filterFrame <- subset(completeData,TIPODEDISCIPLINA == "OBG")
 
+
 # seleciona apenas as colunas que interessam (media, codigo e nome da disciplina e a matricula do aluno)
 filterFrame <- filterFrame[c("CODIGO","MEDIA","MATRICULA")]
 
@@ -27,12 +55,27 @@ filterFrame <- filterFrame[c("CODIGO","MEDIA","MATRICULA")]
 finalFrame = reshape(filterFrame, timevar = "CODIGO", idvar = "MATRICULA", direction = "wide")
 
 # calcula a correla??o
-corMatrix = cor(finalFrame[2:45],use="pairwise.complete.obs")
+corMatrix_spearman = cor(finalFrame[2:45],use="pairwise.complete.obs",method="spearman")
+corMatrix_kendall = cor(finalFrame[2:45],use="pairwise.complete.obs",method="kendall")
 
-write.csv(corMatrix, "matrizCorrelacao.csv")
+write.csv(corMatrix_spearman, "US06_matrizCorrelacao_spearman.csv")
+write.csv(corMatrix_kendall, "US06_matrizCorrelacao_kendall.csv")
 
 
-# graphics
+corMatrix_spearman <- read.csv("US06_matrizCorrelacao_spearman.csv", header = TRUE, sep=",")
+corMatrix_kendall <- read.csv("US06_matrizCorrelacao_kendall.csv", header = TRUE, sep=",")
+
+grade <- read.csv("grade-disciplinas-por-periodo.csv", header = TRUE, sep=",")
+grade = grade[c("DISCIPLINA","CODIGO")]
+
+mdata3 = formataMatrizCorrelacao(corMatrix_spearman,grade,0.5,-0.5)
+mdata4 = formataMatrizCorrelacao(corMatrix_kendall,grade,0.5,-0.5)
+
+write.csv(mdata3, "US06_matrizCorrelacaoFiltrada_spearman.csv",row.names=FALSE)
+write.csv(mdata4, "US06_matrizCorrelacaoFiltrada_kendall.csv",row.names=FALSE)
+
+
+################ graphics
 
 rgb.palette <- colorRampPalette(c("blue", "yellow"), space = "rgb")
 levelplot(corMatrix, xlab="Disciplina", ylab="Alunos", col.regions=rgb.palette(120), width = 900, height = 900, units = "px", cuts=100, at=seq(0,1,0.01))
@@ -40,45 +83,4 @@ levelplot(corMatrix, xlab="Disciplina", ylab="Alunos", col.regions=rgb.palette(1
 
 require(lattice)
 levelplot(corMatrix, xlab="Disciplina", ylab="Alunos", width = 900, height = 900, units = "px")
-
-data <- read.csv("matrizCorrelacao.csv", header = TRUE, sep=",", encoding="latin1")
-data2 = data
-for (i in 2:length(data)) {
-  data2[i-1,2:i] = NA
-}
-data = data2
-grade <- read.csv("grade-disciplinas-por-periodo.csv", header = TRUE, sep=",")
-grade = grade[c("DISCIPLINA","CODIGO")]
-
-library(reshape)
-mdata <- melt(data, id=c("X"))
-mdata$X <- substring(mdata$X, 7)
-mdata$variable <- substring(mdata$variable, 7)
-mdata <- merge(mdata,grade,by.x="X",by.y="CODIGO")
-mdata <- merge(mdata,grade,by.x="variable",by.y="CODIGO")
-
-mdata1 <- subset(mdata,X != variable)
-mdata2<- subset(mdata1,value > 0.55 | value < -0.55)
-mdata2 <- mdata2[with(mdata2, order(-value)), ]
-colnames(mdata2) <- c("cod1","cod2","cor","disc1","disc2")
-mdata3<- subset(mdata2,cor != 1.0 & cor != -1.0)
-
-write.csv(mdata2, "matrizCorrelacaoFiltrada.csv",row.names=FALSE)
-write.csv(mdata3, "matrizCorrelacaoFiltrada1.csv",row.names=FALSE)
-
-teste = subset(notas,coddisciplina %in% c("1307151","1411182"))
-
-teste <- teste[c("coddisciplina","matricula", "media")]
-teste = reshape(teste, timevar = "coddisciplina", idvar = "matricula", direction = "wide")
-
-# calcula a correla??o
-corMatrix = cor(teste[2:3],use="pairwise.complete.obs")
-teste[complete.cases(teste),]
-
-grade2 = read.csv("grade-disciplinas-por-periodo.csv")
-
-a = unique(mdata3[c("cod1", "cod2")])
-aggregate(numeric(nrow(mdata3)), mdata3[c("cod1", "cod2")], length)    # gives a data frame
-
-
-b = data[2+1,2:2]
+#############
